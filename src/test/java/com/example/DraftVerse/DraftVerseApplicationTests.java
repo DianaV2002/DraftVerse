f9bb33695c450660b4a979df1982fdc2fced074a
DianaXWiki <139217939+DianaXWiki@users.noreply.github.com>
2024-11-01 13:50:14 +0200

    Add initial structure

---
+++ src/test/java/com/example/DraftVerse/DraftVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DraftVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
