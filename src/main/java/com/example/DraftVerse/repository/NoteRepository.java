04cabb7caa26e8781cab9fcea56a77c81f50464e
Diana <diana-maria.chelea@student.tuiasi.ro>
2024-11-08 12:59:36 +0200

    Fix project

--- src/main/java/com/example/DraftVerse/repository/NoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.DraftVerse.repository;
-
-import com.example.DraftVerse.persistence.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoteRepository extends JpaRepository<Note, Integer> {
-}
